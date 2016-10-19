2d3e70b90ecc2b498b06c38699d378361dd21ddf
Alexey Novak <alexey.novak.mail@gmail.com>
2016-10-19 14:58:06 -0400

    master: adding bag of words

--- dist/spam-detector.js
+++ dist/spam-detector.js
@@ -4,13 +4,21 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _mailparser = require('mailparser');
 
+var _natural = require('natural');
+
+var _natural2 = _interopRequireDefault(_natural);
+
 var _promise = require('promise');
 
 var _promise2 = _interopRequireDefault(_promise);
+
+require('babel-polyfill');
 
 var _fs = require('fs');
 
@@ -18,10 +26,26 @@ var _fs2 = _interopRequireDefault(_fs);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var BACK_SLASH = '/';
 var MILLISECONDS = 1000;
+
+var MAX_WORD_FEATURES = 10;
+
+var EMAIL_REGEX = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+var URL_REGEX = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+var HTML_REGEX = /<[^<>]+>/;
+var NUMBER_REGEX = /\d+/;
+var DOLLAR_REGEX = /\$\d+(,\d+)*(?:\.\d+)?/g;
+var ALPHA_NUMERIC_REGEX = /[^\w\s]|_/g;
+var PUNCTUATION_REGEX = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+var SINGLE_SPACE_REGEX = /\s+/g;
+
+// extend String methods
+_natural2.default.PorterStemmer.attach();
 
 var SpamDetector = function () {
   function SpamDetector() {
@@ -42,17 +66,41 @@ var SpamDetector = function () {
     this.testPath = testPath;
     this.testData = testData;
     this.trainPath = this._appendSlash(trainPath);
+    this.bagOfWords = new Map();
   }
 
   _createClass(SpamDetector, [{
     key: 'train',
     value: function train() {
+      var _this = this;
+
       var startTime = new Date();
+      var taskTime = startTime;
       console.log('Training starts...');
 
-      this._readEmails(this.trainData).then(function () {
-        var duration = (Date.now() - startTime) / MILLISECONDS;
-        console.log('Training finished. Execution time: ' + duration + ' seconds');
+      console.log('Reading files starts...');
+
+      this._readEmails(this.trainData).then(function (results) {
+        console.log('Results are ' + JSON.stringify(results[0]));
+
+        var duration = (Date.now() - taskTime) / MILLISECONDS;
+        console.log('Reading files finished. Execution time: ' + duration + ' seconds');
+
+        taskTime = new Date();
+        return _this._preprocessEmails(results);
+      }).then(function (results) {
+        console.log('Results are ' + JSON.stringify(results[0]));
+
+        var duration = (Date.now() - taskTime) / MILLISECONDS;
+        console.log('Email preprocess finished. Execution time: ' + duration + ' seconds');
+
+        _this._generateBagOfWords(results);
+
+        return _this._vectorize(results);
+      }).then(function (results) {
+        console.log('Results are ' + JSON.stringify(results[0]));
+      }).catch(function (error) {
+        throw error;
       });
     }
   }, {
@@ -61,9 +109,160 @@ var SpamDetector = function () {
       return path.substr(-1) === BACK_SLASH ? path : '' + path + BACK_SLASH;
     }
   }, {
+    key: '_vectorize',
+    value: function _vectorize() {
+      var tokenizedEmails = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+      return tokenizedEmails;
+    }
+  }, {
+    key: '_generateBagOfWords',
+    value: function _generateBagOfWords() {
+      var _this2 = this;
+
+      var tokenizedEmails = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+      try {
+        var bagOfWords = new Map();
+
+        var _iteratorNormalCompletion = true;
+        var _didIteratorError = false;
+        var _iteratorError = undefined;
+
+        try {
+          for (var _iterator = tokenizedEmails[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var tokenizedEmail = _step.value;
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
+
+            try {
+              for (var _iterator2 = tokenizedEmail.text[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                var token = _step2.value;
+
+                bagOfWords.set(token, bagOfWords.has(token) ? bagOfWords.get(token) + 1 : 1);
+              }
+            } catch (err) {
+              _didIteratorError2 = true;
+              _iteratorError2 = err;
+            } finally {
+              try {
+                if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                  _iterator2.return();
+                }
+              } finally {
+                if (_didIteratorError2) {
+                  throw _iteratorError2;
+                }
+              }
+            }
+          }
+        } catch (err) {
+          _didIteratorError = true;
+          _iteratorError = err;
+        } finally {
+          try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
+          } finally {
+            if (_didIteratorError) {
+              throw _iteratorError;
+            }
+          }
+        }
+
+        var sortedArray = [].concat(_toConsumableArray(bagOfWords.entries())).sort(function (a, b) {
+          return a[1] > b[1] ? -1 : 1;
+        }).slice(0, MAX_WORD_FEATURES);
+
+        sortedArray.forEach(function (element, index) {
+          _this2.bagOfWords.set(element[0], index + 1);
+        });
+
+        console.log(this.bagOfWords);
+      } catch (e) {
+        console.log(e);throw e;
+      }
+    }
+  }, {
+    key: '_preprocessEmails',
+    value: function _preprocessEmails() {
+      var _this3 = this;
+
+      var emails = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+      return new _promise2.default(function (resolve, reject) {
+        try {
+          var results = emails.map(function (email) {
+            return _extends({}, email, {
+              text: _this3._preprocessEmailText(email.text)
+            });
+          });
+
+          resolve(results);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    }
+  }, {
+    key: '_preprocessEmailText',
+    value: function _preprocessEmailText(text) {
+      if (typeof text !== 'string') {
+        return [];
+      }
+
+      var result = text;
+
+      // Preprocess step #1: Lower case
+      result = result.toLowerCase();
+
+      // Preprocess step #2: Strip all HTML
+      result = result.replace(HTML_REGEX, ' ');
+
+      // Preprocess step #3: Handle URLs
+      result = result.replace(URL_REGEX, 'httpaddr');
+
+      // Preprocess step #4: Handle Email addresses
+      result = result.replace(EMAIL_REGEX, 'emailaddr');
+
+      // Preprocess step #5: Handle $ sign
+      result = result.replace(DOLLAR_REGEX, 'dollar');
+
+      // Preprocess step #6: Remove punctuation
+      result = result.replace(ALPHA_NUMERIC_REGEX, ' ');
+
+      // Preprocess step #7: Replace characters between 0-9
+      result = result.replace(NUMBER_REGEX, 'number');
+
+      // Preprocess step #8: Remove extra spaces
+      result = result.replace(SINGLE_SPACE_REGEX, ' ');
+
+      // Preprocess step #9: Stem the words
+      result = result.tokenizeAndStem();
+
+      return result;
+    }
+  }, {
+    key: '_readEmails',
+    value: function _readEmails() {
+      var _this4 = this;
+
+      var emailsData = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+      var promises = emailsData.map(function (emailData) {
+        return _this4._readEmail(emailData);
+      });
+
+      return _promise2.default.all(promises);
+    }
+  }, {
     key: '_readEmail',
-    value: function _readEmail(emailData) {
-      var _this = this;
+    value: function _readEmail() {
+      var _this5 = this;
+
+      var emailData = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
       return new _promise2.default(function (resolve, reject) {
         var mailparser = new _mailparser.MailParser();
@@ -73,39 +272,14 @@ var SpamDetector = function () {
 
 
         mailparser.on('end', function (mailObj) {
-          console.log('Subject: ' + mailObj.subject);
-          //console.log(mailObj);
-
           resolve({
             subject: mailObj.subject,
             text: mailObj.text,
             classifier: classifier
           });
-
-          //this.featuresTrain.push(mailObj.subject);
-          //this.labelsTrain.push(classifier);
         });
 
-        _fs2.default.createReadStream('' + _this.trainPath + name).pipe(mailparser);
-      });
-    }
-  }, {
-    key: '_readEmails',
-    value: function _readEmails(emailsData) {
-      var _this2 = this;
-
-      var startTime = new Date();
-      console.log('Reading files starts...');
-
-      var promises = emailsData.map(function (emailData) {
-        return _this2._readEmail(emailData);
-      });
-
-      return _promise2.default.all(promises).then(function (results) {
-        console.log('Results are ' + JSON.stringify(results));
-
-        var duration = (Date.now() - startTime) / MILLISECONDS;
-        console.log('Reading files finished. Execution time: ' + duration + ' seconds');
+        _fs2.default.createReadStream('' + _this5.trainPath + name).pipe(mailparser);
       });
     }
   }]);
--- src/spam-detector.js
+++ src/spam-detector.js
@@ -1,9 +1,25 @@
 import { MailParser } from 'mailparser';
+import natural from 'natural';
 import Promise from 'promise';
+import 'babel-polyfill';
 import fs from 'fs';
 
 const BACK_SLASH = '/';
 const MILLISECONDS = 1000;
+
+const MAX_WORD_FEATURES = 5000;
+
+const EMAIL_REGEX = /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+const URL_REGEX = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+const HTML_REGEX = /<[^<>]+>/;
+const NUMBER_REGEX = /\d+/;
+const DOLLAR_REGEX = /\$\d+(,\d+)*(?:\.\d+)?/g;
+const ALPHA_NUMERIC_REGEX = /[^\w\s]|_/g;
+const PUNCTUATION_REGEX = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+const SINGLE_SPACE_REGEX = /\s+/g;
+
+// extend String methods
+natural.PorterStemmer.attach();
 
 export default class SpamDetector {
   constructor(options = defaultOptions) {
@@ -21,16 +37,42 @@ export default class SpamDetector {
     this.testPath = testPath;
     this.testData = testData;
     this.trainPath = this._appendSlash(trainPath);
+    this.bagOfWords = new Map();
   }
 
   train() {
     const startTime = new Date();
+    let taskTime = startTime;
     console.log('Training starts...');
 
-    this._readEmails(this.trainData).then(() => {
-      const duration = (Date.now() - startTime) / MILLISECONDS;
-      console.log(`Training finished. Execution time: ${duration} seconds`);
-    });
+    console.log('Reading files starts...');
+
+    this._readEmails(this.trainData)
+      .then((results) => {
+        console.log(`Results are ${JSON.stringify(results[0])}`);
+
+        const duration = (Date.now() - taskTime) / MILLISECONDS;
+        console.log(`Reading files finished. Execution time: ${duration} seconds`);
+
+        taskTime = new Date();
+        return this._preprocessEmails(results);
+      })
+      .then((results) => {
+        console.log(`Results are ${JSON.stringify(results[0])}`);
+
+        const duration = (Date.now() - taskTime) / MILLISECONDS;
+        console.log(`Email preprocess finished. Execution time: ${duration} seconds`);
+
+        this._generateBagOfWords(results);
+
+        return this._vectorize(results);
+      })
+      .then((results) => {
+        console.log(`Results are ${JSON.stringify(results[0])}`);
+      })
+      .catch(error => {
+        throw error;
+      });
   }
 
   _appendSlash(path) {
@@ -39,7 +81,92 @@ export default class SpamDetector {
       : `${path}${BACK_SLASH}`;
   }
 
-  _readEmail(emailData) {
+  _vectorize(tokenizedEmails = []) {
+    return tokenizedEmails;
+  }
+
+  _generateBagOfWords(tokenizedEmails = []) {
+    let bagOfWords = new Map();
+
+    for (const tokenizedEmail of tokenizedEmails) {
+      for (const token of tokenizedEmail.text) {
+        bagOfWords.set(token, bagOfWords.has(token)
+         ? bagOfWords.get(token) + 1
+         : 1
+        );
+      }
+    }
+
+    const sortedArray = [...bagOfWords.entries()]
+      .sort((a, b) => {
+        return a[1] > b[1] ? -1 : 1;
+      })
+      .slice(0, MAX_WORD_FEATURES);
+
+    sortedArray.forEach((element, index) => {
+      this.bagOfWords.set(element[0], index + 1)
+    });
+  }
+
+  _preprocessEmails(emails = []) {
+    return new Promise((resolve, reject) => {
+      try {
+        const results = emails.map(email => ({
+          ...email,
+          text: this._preprocessEmailText(email.text)
+        }));
+
+        resolve(results);
+      } catch(error) {
+        reject(error);
+      }
+    });
+  }
+
+  _preprocessEmailText(text) {
+    if (typeof text !== 'string') {
+      return [];
+    }
+
+    let result = text;
+
+    // Preprocess step #1: Lower case
+    result = result.toLowerCase();
+
+    // Preprocess step #2: Strip all HTML
+    result = result.replace(HTML_REGEX, ' ');
+
+    // Preprocess step #3: Handle URLs
+    result = result.replace(URL_REGEX, 'httpaddr');
+
+    // Preprocess step #4: Handle Email addresses
+    result = result.replace(EMAIL_REGEX, 'emailaddr');
+
+    // Preprocess step #5: Handle $ sign
+    result = result.replace(DOLLAR_REGEX, 'dollar');
+
+    // Preprocess step #6: Remove punctuation
+    result = result.replace(ALPHA_NUMERIC_REGEX, ' ');
+
+    // Preprocess step #7: Replace characters between 0-9
+    result = result.replace(NUMBER_REGEX, 'number');
+
+    // Preprocess step #8: Remove extra spaces
+    result = result.replace(SINGLE_SPACE_REGEX, ' ');
+
+    // Preprocess step #9: Stem the words
+    result = result.tokenizeAndStem();
+
+    return result;
+  }
+
+  _readEmails(emailsData = []) {
+    const promises = emailsData.map(emailData => this._readEmail(emailData));
+
+    return Promise.all(promises);
+  }
+
+  _readEmail(emailData = {}) {
     return new Promise((resolve, reject) => {
       const mailparser = new MailParser();
 
@@ -56,20 +183,5 @@ export default class SpamDetector {
       fs.createReadStream(`${this.trainPath}${name}`)
         .pipe(mailparser);
     });
-  }
-
-  _readEmails(emailsData) {
-    const startTime = new Date();
-    console.log('Reading files starts...');
-
-    const promises = emailsData.map(emailData => this._readEmail(emailData));
-
-    return Promise.all(promises)
-      .then(results => {
-        console.log(`Results are ${JSON.stringify(results)}`);
-
-        const duration = (Date.now() - startTime) / MILLISECONDS;
-        console.log(`Reading files finished. Execution time: ${duration} seconds`);
-      });
   }
 }
